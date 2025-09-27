6eccc1fc7d4e13f982eb520e4f33c5a578da4daa
Yashveer singh <yashveersingh1921@gmail.com>
2025-09-27 19:39:57 +0530

    Recoil

--- week-7/Context API/src/App.jsx
+++ week-7/Context API/src/App.jsx
@@ -1,28 +1,39 @@
-import React, { useState } from 'react'
-
+import React from "react";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
+//npm install react@18.3.1 react-dom@18.3.1
 const App = () => {
-  const [count, setCount] = useState(0)
   return (
     <div>
-      <Count count={count} setCount={setCount} />
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
     </div>
   )
-  function Count(){
-    return <div>
-      <CountRenderer count={count} />
-      <Button count={count} setCount={setCount} />
-    </div>
-
-  }
-  function CountRenderer({count}) {
+}
+function Count(){
+  return <div>
+    <CountRenderer />
+    <Button />
+  </div>
+}
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>
     {count}
-  }
-  function Button({count, setCount}){
-    return <div>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-    </div>
-  }
+    <EvenOdd />
+  </div>
+}
+function EvenOdd(){
+  const isEven = useRecoilValue(evenSelector);
+  return <div>{(isEven % 2 == 0)? "it is even":"it is odd"}</div>
+}
+function Button(){
+  const setCount = useSetRecoilState(countAtom);
+  return <div>
+    <button onClick={() => setCount(count => count + 1)}>Increase</button>
+    <button onClick={() => setCount(count => count - 1)}>Decrease</button>
+  </div>
 }
 
 export default App
