ff0cd817286e46f36aa47b667dec364cc181b272
Yashveer singh <yashveersingh1921@gmail.com>
2026-01-31 23:35:27 +0530

    auto refreshing hook

--- week-9/vite-project/src/App.jsx
+++ week-9/vite-project/src/App.jsx
@@ -1,31 +1,43 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function useTodos() {
+function useTodos(n) {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("https://dummyjson.com/todos")
+    const value = setInterval(() => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+        .then(res => {
+          setTodos(res.data.todos);
+          setLoading(false);
+        })
+    }, n * 1000)
+  
+    axios.get("https://sum-server.100xdevs.com/todos")
       .then(res => {
         setTodos(res.data.todos);
         setLoading(false);
       })
-  }, [])
 
-  return { todos, loading };
+    return () => {
+      clearInterval(value)
+    }
+  }, [n])
+
+  return {todos, loading};
 }
 
 function App() {
-  const { todos, loading } = useTodos();
+  const {todos, loading} = useTodos(10);
 
-  if(loading){
-    return <div>Loading...</div>
+  if (loading) {
+    return <div> loading... </div>
   }
 
   return (
     <>
-      {todos.map(todo => <Track key={todo.id} todo={todo} />)}
+      {todos.map(todo => <Track todo={todo} />)}
     </>
   )
 }
