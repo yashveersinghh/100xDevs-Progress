0dd0a7c3f8cc27848866b5404767b237e3fc6acd
Yashveer singh <yashveersingh1921@gmail.com>
2026-03-27 23:41:19 +0530

    apis

--- week-12/ts-apis/src/index.ts
+++ week-12/ts-apis/src/index.ts
@@ -1,12 +1,28 @@
 interface User {
+    id: string;
     name: string;
     age: number;
+    email: string;
+    password: string;
+}
+//PICK
+type updateProps = Pick<User, 'name' | 'age' | 'email'>;
+//PARTIAL
+type updatePropsOptional = Partial<updateProps>;
+
+function updateUser(user: updatePropsOptional) {
+    //hit
 }
 
-function sumOfAge(user1: User, user2: User): number {
-    return user1.age + user2.age;
+//READONLY
+type User2 = {
+    username: string; //readonly specifically
+    userage: number;
 }
 
-const age = sumOfAge({ name: "Alice", age: 30 }, { name: "Bob", age: 25 });
+const user: Readonly<User2> = {
+    username: 'yash',
+    userage: 22
+}
 
-console.log(age);
+user.userage=21
