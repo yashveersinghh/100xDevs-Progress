6d919ec634af7c1ffd2c70f2623edc94d92674ef
Yashveer singh <yashveersingh1921@gmail.com>
2026-03-28 23:26:16 +0530

    type inference in zod

--- week-12/ts-apis/src/index.ts
+++ week-12/ts-apis/src/index.ts
@@ -25,4 +25,41 @@ const user: Readonly<User2> = {
     userage: 22
 }
 
-user.userage=21
+// user.userage=21 //error because user is readonly
+
+//RECORD
+type userAge = {
+    [key: string]: number
+}
+
+type Users = Record<string, {age: number; name: string}>
+
+const user2: Users = {
+    "yash": {
+        age: 22,
+        name: "yash"
+    },
+    "yash@": {
+        age: 3,
+        name: "yash@"
+    }
+}
+
+//MAP
+const users = new Map<string, {age: number; name: string}>();
+users.set("yash", {age: 22, name: "yash"});
+users.set("yash@", {age: 3, name: "yash@"});
+
+const first = users.get("yash");
+
+//EXCLUDE
+type eventType = 'click' | 'scroll' | 'mousemove';
+type MouseEvent = Exclude<eventType, 'scroll'>; //click | mousemove
+
+const eventHandle = (event: MouseEvent) => {
+    //handle click and mousemove events
+    console.log(`${event}`)
+}
+
+eventHandle('click'); // works
+// eventHandle('scroll'); // error because scroll is excluded
